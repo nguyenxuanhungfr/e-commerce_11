643060bfcb617e9328d72ea70e4854a89926b183
hungxuan <nguyen.xuan.hung@framgia.com>
2018-05-24 17:33:59 +0700

    Update cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,20 +11,25 @@ module ApplicationHelper
   def total_cart products
     total = 0
     products.each do |product,quantity|
-      total += product.new_price*quantity
+      total += product.new_price * quantity
     end
     total
   end
 
   def gravatar_for product
-    image_tag product.images,alt: product.name
+    if product.images.first.nil?
+      image_tag "https://qph.fs.quoracdn.net/main-qimg-680c8f445130201b7b1850e7d02d76dd-c",
+        alt: product.name
+    else
+      image_tag product.images.first.image_url, alt: product.name
+    end
   end
 
-  def get_user_images comment
-    if comment.images.present?
-      image = comment.images
-    else
+  def load_user_images comment
+    if comment.image.blank?
       image = "https://png.icons8.com/dotty/2x/administrator-male.png"
+    else
+      image = comment.image
     end
     image_tag image
   end
