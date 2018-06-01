8185597b0a615ef3c19d0992be104a22aea86dc2
hungxuan <nguyen.xuan.hung@framgia.com>
2018-06-01 08:14:18 +0700

    manage_comments

--- app/assets/javascripts/admin/dashboard.js
+++ app/assets/javascripts/admin/dashboard.js
@@ -571,4 +571,24 @@ function handleChange(input) {
   if (input.value < 0) input.value = 0;
   if (input.value > 99) input.value = 99;
 }
+$(document).ready(function() {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
 
+      reader.onload = function(e) {
+        $('.view-img img').attr('src', e.target.result);
+      }
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#user_picture").change(function() {
+    readURL(this);
+  });
+
+  $('.user_picture').on('click', function() {
+    $('#user_picture').trigger('click');
+
+  });
+});
